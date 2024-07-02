422582e3aa846a4dad17028bc268934d38d9a766
Instantf7 <138810527+Instantf7@users.noreply.github.com>
2024-07-02 14:43:29 -0700

    Update botkiller.js

--- botkill.js
+++ botkill.js
@@ -1,19 +1,52 @@
-
-        (function() {
-            const maxRefreshes = 30;
-            const storageKey = 'pageRefreshCount';
-
-            // Get the stored refresh count from localStorage
-            let refreshData = JSON.parse(localStorage.getItem(storageKey)) || { count: 0 };
-
-            // Increment the refresh count
-            refreshData.count += 1;
-
-            // Store the updated refresh data in localStorage
-            localStorage.setItem(storageKey, JSON.stringify(refreshData));
-
-            // Check if the number of page refreshes exceeds the maxRefreshes
-            if (refreshData.count > maxRefreshes) {
-                window.location.href = '/blocked.1';
-            }
-        })();
+(function(){
+    function a(b,c,d){
+        const e=new Date();
+        e.setTime(e.getTime()+(d*60*1000));
+        const f="expires="+e.toUTCString();
+        document.cookie=b+"="+c+";"+f+";path=/";
+    }
+    function g(b){
+        const c=b+"=";
+        const d=document.cookie.split(';');
+        for(let e=0;e<d.length;e++){
+            let f=d[e];
+            while(f.charAt(0)==' ')f=f.substring(1,f.length);
+            if(f.indexOf(c)==0)return f.substring(c.length,f.length);
+        }
+        return null;
+    }
+    (function(){
+        const h=30;
+        const i=60;
+        const j='pageRefreshCount';
+        let k=JSON.parse(localStorage.getItem(j))||{count:0};
+        k.count+=1;
+        if(k.count>=i)k.count=0;
+        localStorage.setItem(j,JSON.stringify(k));
+        if(k.count>h){
+            a('blocked','true',5);
+            window.location.href='/blocked.1';
+        }
+        if(g('blocked')==='true'){
+            window.location.href='/blocked.1';
+        }
+    })();
+    (function(){
+        const l=20;
+        const m='pageTabCount';
+        let n=JSON.parse(localStorage.getItem(m))||{count:0};
+        n.count+=1;
+        localStorage.setItem(m,JSON.stringify(n));
+        if(n.count>l){
+            a('blocked','true',5);
+            window.location.href='/blocked.1';
+        }
+        window.addEventListener('beforeunload',function(){
+            n.count-=1;
+            localStorage.setItem(m,JSON.stringify(n));
+        });
+        if(g('blocked')==='true'){
+            window.location.href='/blocked.1';
+        }
+    })();
+})();
