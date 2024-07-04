ac7942a3b93f7fbc3094df3b804b43a9a8b2daf6
BLOCK <138810527+Instantf7@users.noreply.github.com>
2024-07-04 00:24:16 -0700

    Delete null.js

--- null.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    try {
-        localStorage.setItem('test', 'test');
-        localStorage.removeItem('test');
-    } catch (e) {
-        window.location.href = './blocked.1';
-    }
-})();
